268f196ba3ed577f54e20f0cd610cc992291a1d7
baynejen <bayne.jen@gmail.com>
2016-02-07 23:46:56 +0000

    portfolio

---
+++ portfolio/contactthanks.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jen Bayne | Contact</title>
+	<link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'/ >
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="script2.js"></script>
+	<!--[if IE]>
+  	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div id="site-wrapper">
+
+<!-- NAV -->
+	
+		<nav>
+			<div>
+				<a href="index.html"><img src="images/logo/logo2.png" alt="logo" class="logo"></a>
+				<img src="images/line.png" alt="line">
+			</div>
+			<ul>
+				<li><a href="index.html">Portfolio</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="#" class="highlighted">Contact</a></li>
+			</ul>
+			<img src="images/line.png" alt="line">
+			<div id="footer">
+				<div id="social">
+					<a href="https://twitter.com/Jen_Bayne" target="_blank"><img src="images/social/twitter.png" alt="twitter logo"></a>
+					<a href="mailto:bayne.jen@gmail.com" target="_blank"><img src="images/social/email.png" alt="email logo"></a>
+					<a href="https://www.instagram.com/jenbayne/" target="_blank"><img src="images/social/instagram.png" alt="instagram logo"></a>
+				</div>
+				<div>
+					<p>Copyright &copy; 2016 Jen Bayne
+				</div>
+			</div>
+		</nav>
+
+
+<!-- MOBILE NAV -->
+
+		<div id="mobile-nav">
+			<div>
+				<a href="index.html"><img src="images/logo/logo2.png" alt="logo"></a>
+			</div>
+			<div id='cssmenu'>
+				<ul>
+				<li>
+					<img src="images/line.png" alt="line"><img src="images/line.png" alt="line" class="line2"></li>
+				   	<li><a href="index.html">Portfolio</a></li>
+				   	<li><a href="about.html">About</a></li>
+				   	<li><a href="#" class="highlighted-mobile">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+
+<!-- CONTACT -->
+		
+		<div id="body-wrapper">
+			<div class="container" id="contact">
+				<p>Your message has been sent, thank you! I will get back to you as soon as I can.</p>
+			</div>
+		</div>
+
+<!-- MOBILE FOOTER -->		
+
+		<div id="mobile-footer">
+			<div id="mobile-social">
+				<a href="https://twitter.com/Jen_Bayne" target="_blank"><img src="images/social/twitter.png" alt="twitter logo"></a>
+				<a href="mailto:bayne.jen@gmail.com" target="_blank"><img src="images/social/email.png" alt="email logo"></a>
+				<a href="https://www.instagram.com/jenbayne/" target="_blank"><img src="images/social/instagram.png" alt="instagram logo"></a>
+			</div>
+			<div>
+				<p>Copyright &copy; 2016 Jen Bayne
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
