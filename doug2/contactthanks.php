710dbd381db02956963f72c4914035fb4ec32bd1
baynejen <bayne.jen@gmail.com>
2016-04-16 19:07:57 +0100

    doug

---
+++ doug2/contactthanks.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contact | Sforzando</title>
+	<link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'/ >
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<!--[if IE]>
+  	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="page-wrapper">	
+<!-- nav -->
+		<nav>
+			<div class="nav-content clearfix">
+				<div class="logo">
+					<h1><a href="index.html">Sforzando</a></h1>
+					<p>Classical Soprano and Piano/Organ Duo</p>
+				</div>
+				<div class="nav-links">
+					<ul>
+						<a href="index.html"><li>Home</li></a>
+						<a href="about.html" ><li>Who we are</li></a>
+						<a href="gallery.html"><li>Gallery</li></a>
+						<a href="audio.html"><li>Audio and video</li></a>
+						<a href="#" class="highlighted"><li>Contact</li></a>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+
+<!-- MOBILE NAV -->
+
+		<div id="mobile-nav">
+			<div>
+				<h1><a href="index.html">Sforzando</a></h1>
+			</div>
+
+			<div id='cssmenu'>
+				<ul>
+				   <li><a href="index.html">Home</a></li>
+				   <li><a href="about.html">Who we are</a></li>
+				   <li><a href="gallery.html">Gallery</a></li>
+				   <li><a href="audio.html" >Audio and video</a></li>
+				   <li><a href="#" class="highlighted">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+
+
+<!-- body -->
+	
+		<div class="body-wrapper">
+			<div class="content-wrapper clearfix">
+				<div id="contact-thanks">
+					<h2>Thank you!</h2>
+					<p>Your message has now been sent and we'll get back to you as soon as possible.</p>
+				</div>
+			</div>
+		</div>
+
+<!-- FOOTER -->
+	
+		<footer>
+			<div class="footer-info">
+				<p>&copy; 2016 Douglas Holligan and Madeleine Kearns. Website by <a href="http://www.jen-bayne.com" target="_blank">Jen Bayne</a>.</p>
+			</div>
+			<div class="social">
+				<a href="#" target="_blank"><img src="images/facebook.png"></a>
+			</div>
+		</footer>
+	</div>
+
+</body>
+</html>
