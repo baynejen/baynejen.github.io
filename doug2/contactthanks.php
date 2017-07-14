386716907c9ffded889a7842ee4c7915eefa425b
Jen Bayne <bayne.jen@gmail.com>
2017-07-14 10:05:56 +0100

    fixes

--- doug2/contactthanks.php
+++ doug2/contactthanks.php
@@ -7,9 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
 	<!--[if IE]>
@@ -17,49 +16,45 @@
 	<![endif]-->
 </head>
 <body>
-	<div class="page-wrapper">	
-<!-- nav -->
+<!-- OFFSITE NAVIGATION -->	
+
+	<ul class="offsite-navigation">
+	   	<li class="offsite-nav-item"><a href="index.html">Home</a></li>
+	    <li class="offsite-nav-item"><a href="about.html">About</a></li>
+	    <li class="offsite-nav-item"><a href="gallery.html">Gallery</a></li>
+	    <li class="offsite-nav-item"><a href="audio.html">Audio and Video</a></li>
+	    <li class="offsite-nav-item offsite-nav-item-selected"><a href="#">Contact</a></li>
+	</ul>
+
+	<input type="checkbox" id="nav-trigger" class="nav-trigger" />
+	<label for="nav-trigger"></label>
+
+	<div class="wrapper clearfix">
+
+<!-- NAV -->
+
 		<nav>
 			<div class="nav-content clearfix">
 				<div class="logo">
-					<h1><a href="index.html">Sforzando</a></h1>
-					<p>Classical Soprano and Piano/Organ Duo</p>
+					<a href="index.html"><img src="images/logo-new.png" alt="sforzando logo"></a>
+					<p>Classical Soprano and Piano-Organ Duo</p>
 				</div>
 				<div class="nav-links">
-					<ul>
-						<a href="index.html"><li>Home</li></a>
-						<a href="about.html" ><li>Who we are</li></a>
-						<a href="gallery.html"><li>Gallery</li></a>
-						<a href="audio.html"><li>Audio and video</li></a>
-						<a href="#" class="highlighted"><li>Contact</li></a>
+					<ul>	
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>				
+						<li><a href="gallery.html">Gallery</a></li>
+						<li><a href="audio.html">Audio and Video</a></li>
+						<li><p>Contact</p></li>
+
 					</ul>
 				</div>
 			</div>
 		</nav>
 
-
-<!-- MOBILE NAV -->
-
-		<div id="mobile-nav">
-			<div>
-				<h1><a href="index.html">Sforzando</a></h1>
-			</div>
-
-			<div id='cssmenu'>
-				<ul>
-				   <li><a href="index.html">Home</a></li>
-				   <li><a href="about.html">Who we are</a></li>
-				   <li><a href="gallery.html">Gallery</a></li>
-				   <li><a href="audio.html" >Audio and video</a></li>
-				   <li><a href="#" class="highlighted">Contact</a></li>
-				</ul>
-			</div>
-		</div>
-
-
-<!-- body -->
+<!-- BODY -->
 	
-		<div class="body-wrapper">
+		<<div class="body-wrapper">
 			<div class="content-wrapper clearfix">
 				<div id="contact-thanks">
 					<h2>Thank you!</h2>
@@ -71,14 +66,21 @@
 <!-- FOOTER -->
 	
 		<footer>
-			<div class="footer-info">
-				<p>&copy; 2016 Douglas Holligan and Madeleine Kearns. Website by <a href="http://www.jen-bayne.com" target="_blank">Jen Bayne</a>.</p>
-			</div>
-			<div class="social">
-				<a href="#" target="_blank"><img src="images/facebook.png"></a>
+			<div class="footer-container">
+				<div class="footer-links">
+					<ul>
+						<li><a href="https://www.facebook.com/sforzando.org/?ref=page_internal" target="_blank"><img src="images/facebook.png" alt="facebook"></a></li>
+						<li><a href="about.html">About Sforzando</a></li>
+						<li><a href="gallery.html">Gallery</a></li>
+						<li><a href="audio.html">Audio and video</a></li>
+						<li><a href="contact.html">Contact us</a></li>
+					</ul>
+				</div>
+				<div class="footer-info">
+					<p>&copy; 2017 Douglas Holligan and Madeleine Kearns. Website by <a href="http://www.jen-bayne.com" target="_blank">Jen Bayne</a>.</p>
+				</div>
 			</div>
 		</footer>
 	</div>
-
 </body>
 </html>
