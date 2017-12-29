a0f0876240b3cdce3a8657c2078343989c88bacd
Jen Bayne <bayne.jen@gmail.com>
2017-12-29 17:50:02 +0000

    Update to contact thanks page title

--- portfolio/contactthanks.php
+++ portfolio/contactthanks.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Jen Bayne | Contact</title>
+	<title>Thank You | Jen Bayne</title>
 	<link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'/ >
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
