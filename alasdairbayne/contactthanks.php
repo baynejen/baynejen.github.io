419c249420a97a67cd70acdebfd64e5ba4e43fb5
Jen Bayne <jen.bayne@trouva.com>
2018-06-10 22:32:55 +0100

    further styling changes

--- alasdairbayne/contactthanks.php
+++ alasdairbayne/contactthanks.php
@@ -16,85 +16,60 @@
 <body>
 	<div class="site-wrapper">
 
-
 <!-- NAV -->
 
-
-		<div id="homepage-nav-wrapper">
-		<div class="nav-wrapper">
-			<div class="nav-contents">
-				<nav class="clearfix">
-					<div class="logo"><a href="/"><h1>Alasdair Bayne</h1></a>
-						<p>Independent Director and Freelance Editor</p>
-					</div>
-					<div class="nav-links">
-						<ul>
-							<li><a href="drama">My Work</a></li>
-							<li><a href="about">About</a></li>
-							<li><a href="contact" class="highlighted">Contact</a></li>
-						</ul>
-					</div>
-				</nav>
+		<nav class="clearfix nav-wrapper">
+			<div class="logo">
+				<a href="index"><h1>Alasdair Bayne</h1></a>
+				<p>Independent Director and Freelance Editor</p>
 			</div>
-		</div>
-		</div>
-
-
-<!-- NAV (responsive) -->
-
-
-		<div id="homepage-nav-wrapper-responsive">
-			<div class="nav-mountain">
-				<img src="images/base/mountain-top-small.png">
+			<div class="nav-links">
+				<ul>
+					<li><a href="drama">My Work</a></li>
+					<li><a href="about">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
 			</div>
-			<div class="nav-contents">
-				<nav class="clearfix">
-					<div class="logo"><a href="/"><h1>Alasdair Bayne</h1></a>
-					</div>
-					<div class="nav-links">
-						<ul>
-							<li><a href="drama">My Work</a></li>
-							<li><a href="about">About</a></li>
-							<li><a href="contact" class="highlighted">Contact</a></li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</div>
-
+		</nav>
 
 <!-- BODY -->
 
-
 		<div class="body-wrapper">
-			<div class="content-wrapper">
-				<h2>Thank you</h2>
-				<hr>
-				<p class="contact-subtitle thanks">Your message has been sent. I will get back to you as soon as I can.</p>
-				
+			<div class="body-container">
+				<div class="content-wrapper">
+					<h2>Thank you</h2>
+					<p class="contact-subtitle thanks">Your message has been sent. I will get back to you as soon as I can.</p>
+				</div>
 			</div>
 		</div>
 
 
 <!-- FOOTER -->		
 
-
-		<div class="footer-wrapper">
-			<div class="footer-contents">		
-				<div class="social-container" class="clearfix">
-					<div class="icon">
-						<a href="https://vimeo.com/alibayne"><img src="images/base/vimeo-icon.png"></a>
-					</div>		
-					<div class="icon">
-						<a href="http://alasdairbayne.tumblr.com/"><img src="images/base/tumblr-icon.png"></a>
-					</div>	
-				</div>
-				<div class="copyright">
-					<p>© 2018 Alasdair Bayne. Website by <a href="http://jen-bayne.com/" target="_blank">Jen Bayne</a></p>
-				</div>	
+		
+		<div class="partners">
+			<div class="partners-container clearfix">
+				<img src="images/about/partners/raindance.jpg">
+				<img src="images/about/partners/encounters.jpg">
+				<img src="images/about/partners/sftn.png">
+				<img src="images/about/partners/bafta.jpg">
+				<img src="images/about/partners/cs.jpg">
+				<img src="images/about/partners/inscript.jpg">
 			</div>
 		</div>
-
+		<footer>
+			<div class="social-container" class="clearfix">
+				<div class="icon">
+					<a href="https://vimeo.com/alibayne"><img src="images/base/vimeo-icon.png"></a>
+				</div>		
+				<div class="icon">
+					<a href="http://alasdairbayne.tumblr.com/"><img src="images/base/tumblr-icon.png"></a>
+				</div>	
+			</div>
+			<div class="copyright">
+				<p>© 2018 Alasdair Bayne. Website by <a href="http://jen-bayne.com/" target="_blank">Jen Bayne</a></p>
+			</div>	
+		</footer>
 	</div>
 </body>
 </html>
