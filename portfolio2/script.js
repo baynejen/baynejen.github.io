d9bcc9ed99ebb62dad7b388285c42c7adc4920fd
Jen Bayne <bayne.jen@gmail.com>
2019-02-09 15:02:40 +0000

    update to core styling

---
+++ portfolio2/script.js
@@ -0,0 +1,81 @@
+// FADE CONTENT IN 
+
+// https://codepen.io/bramus/pen/yikfd
+
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+jQuery(function($) {
+    // Function which adds the 'animated' class to any '.animatable' in view
+    var doAnimations = function() {
+      // Calc current offset and get all animatables
+      var offset = $(window).scrollTop() + $(window).height(),
+          $animatables = $('.animatable');
+      // Unbind scroll handler if we have no animatables
+      if ($animatables.length == 0) {
+        $(window).off('scroll', doAnimations);
+      }
+      // Check all animatables and animate them if necessary
+          $animatables.each(function(i) {
+         var $animatable = $(this);
+              if (($animatable.offset().top + $animatable.height() - 200) < offset) {
+          $animatable.removeClass('animatable').addClass('animated');
+              }
+      });
+    };
+    // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+    $(window).trigger('scroll');
+  }
+);
+
+
+// SMOOTH SCROLL OF MENU\
+
+$(document).ready(function(){
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 400, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+});
+    
+// MENU SCROLL TO POSITION
+
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+    
+    if (scrollTop >= 600) {
+      $('header').addClass('scrolled-nav');
+    } else if (scrollTop < 600) {
+      $('header').removeClass('scrolled-nav');
+    } 
+    
+  }); 
+  
+});
+
+
+// SIDE NAV
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("hero-anchor").style.overflow = "hidden";
+  document.getElementById("sticky").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("hero-anchor").style.overflow = "auto";
+  document.getElementById("sticky").style.display = "block";
+}
